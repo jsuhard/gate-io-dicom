fd95f9ffe16448411df5927a4f852f3c82fa6cb0
Jérôme Suhard <jerome@suhard.fr>
2014-10-20 17:36:31 +0200

    Fix compilation error with Geant4 9.5 where a member of the G4Vmodel class was not yet available.

--- source/general/src/GateModel.cc
+++ source/general/src/GateModel.cc
@@ -2,9 +2,12 @@
 
 #include "G4VisManager.hh"
 #include "G4VSolid.hh"
+#include "G4Version.hh"
 
 GateModel::GateModel(const G4VSolid & solid, const G4Transform3D & trans, const G4VisAttributes & att, const G4String category) : solid(solid), att(att), trans(trans) {
+#if G4VERSION_NUMBER >= 960
     fType = "G4"+ category +"Model";
+#endif
     fGlobalTag = solid.GetName();
     fGlobalDescription = fGlobalTag;
 }
